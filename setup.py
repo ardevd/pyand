1108828ee5b74bd3d63d87da4e84e7069bedae67
Untouchab1e <edvard.holst@gmail.com>
2014-03-31 09:44:26 +0200

    Added setup.py script.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pyand",
+    version = "0.9.0",
+    author = "Edvard Holst",
+    author_email = "edvard.holst@gmail.com",
+    description = ("Python wrapper for ADB and Fastboot"),
+    license = "MIT",
+    keywords = "python android adb fastboot",
+    url = "https://github.com/Zyg0te/pyand",
+    packages=['pyand'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+    ],
+)
