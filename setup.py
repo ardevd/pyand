56ac7043eac21dd9b0efd3bc1e96807f5e7b1837
ardevd <edvard.holst@gmail.com>
2017-06-06 01:16:15 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ def read(fname):
 setup(
     name = "pyand",
     version = "0.9.1.2",
-    author = "Edvard Holst",
-    author_email = "edvard.holst@gmail.com",
+    author = "ardevd",
+    author_email = "no-reply@unknown.com",
     description = ("Python wrapper for ADB and Fastboot"),
     license = "MIT",
     keywords = "python android adb fastboot",
-    url = "https://github.com/Zyg0te/pyand",
+    url = "https://github.com/ardevd/pyand",
     packages=['pyand'],
     long_description=read('README.rst'),
     classifiers=[
